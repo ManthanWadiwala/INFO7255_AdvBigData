3ad3a0c89bfa452806bb566cada84b9dea47972f
Manthan Wadiwala <manthancar@gmail.com>
2023-02-10 18:26:01 -0500

    Added Demo-1

---
+++ info7255-demo-1/src/main/java/com/info7255/demo/Info7255DemoApplication.java
@@ -0,0 +1,13 @@
+package com.info7255.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Info7255DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Info7255DemoApplication.class, args);
+	}
+
+}
---
+++ info7255-demo-1/src/main/java/com/info7255/demo/service/JsonValidator.java
@@ -0,0 +1,23 @@
+package com.info7255.demo.service;
+
+import com.info7255.demo.controller.PlanController;
+
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.ValidationException;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonValidator {
+    public void validateJSON(JSONObject jsonObject) throws ValidationException {
+
+        JSONObject jsonSchema = new JSONObject(
+                new JSONTokener(PlanController.class.getResourceAsStream("/planSchema.json")));
+
+        Schema schema = SchemaLoader.load(jsonSchema);
+        schema.validate(jsonObject);
+
+    }
+}
